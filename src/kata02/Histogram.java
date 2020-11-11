b18c71ef76b15fd267fb5f82fea28c16d30fbfac
RaulVF007 <raulvf007@gmail.com>
2020-11-11 09:03:35 +0000

    Kata2 version4.

--- src/kata02/Histogram.java
+++ src/kata02/Histogram.java
@@ -3,25 +3,21 @@ package kata02;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Histogram {
-    public final int[] data;
+public class Histogram<T> {
+    public final T[] data;
     
-    public Histogram(int[] data){
+    public Histogram(T[] data){
         this.data = data;
     }
 
-    public int[] getData() {
+    public T[] getData() {
         return data;
     }
     
-    public Map<Integer,Integer> getHistogram(){
-        Map<Integer,Integer> histogram = new HashMap<>();
-        for (int i = 0; i < data.length; i++) {	        
-            if(histogram.containsKey(data[i])){	            
-                histogram.put(data[i], histogram.get(data[i])+1);	
-            }else{	
-                histogram.put(data[i], 1);	
-            }	
+    public Map<T,Integer> getHistogram(){
+        Map<T,Integer> histogram = new HashMap<>();
+        for (T key : data){
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key)+1 :1);
         }
         return histogram;
     } 
--- src/kata02/Kata02.java
+++ src/kata02/Kata02.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Kata02 {
 
     public static void main(String[] args) {
-        int[] data = {2, 4, 5, 2, 7, 4, 4, 8, 8, 5, 2, 6, 5, 7, 2};
+        Integer[] data = {2, 4, 5, 2, 7, 4, 4, 8, 8, 5, 2, 6, 5, 7, 2};
         Histogram histo = new Histogram(data);
         Map<Integer,Integer > histogr = histo.getHistogram();
         
